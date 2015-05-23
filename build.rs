7640b3a2df6a154c6f78794d430fff96a63d6771
Matt Coffin <mcoffin13@gmail.com>
2015-05-22 22:38:15 -0700

    build: Fix build script for testing

--- build.rs
+++ build.rs
@@ -3,18 +3,17 @@ use std::fs;
 use std::io;
 use std::path::Path;
 
-fn get_platform() -> String {
+fn get_platform() -> Option<String> {
     let features = env::vars().filter(|&(ref key, _)| key.starts_with("CARGO_FEATURE_"));
-    let (feature_var, _) = features.last().expect("No platform specified");
-    feature_var.trim_left_matches("CARGO_FEATURE_").to_string()
+    match features.last() {
+        Some((feature_var, _)) => Some(feature_var.trim_left_matches("CARGO_FEATURE_").to_string()),
+        None => None,
+    }
 }
 
-fn copy_linker_scripts<P: AsRef<Path>>(out_path: P) -> io::Result<()> {
-    // Get the name of the MCU for which we're compiling
-    let target = get_platform();
-
+fn copy_linker_scripts<P: AsRef<Path>, Q: AsRef<Path>>(target: P, out_path: Q) -> io::Result<()> {
     // Try copying the linker scripts
-    let target_dir = Path::new("src/hal").join(&target);
+    let target_dir = Path::new("src/hal").join(target);
     let out_dir: &Path = out_path.as_ref();
     try!(fs::copy("src/hal/layout_common.ld", out_dir.join("layout_common.ld")));
     try!(fs::copy(target_dir.join("iomem.ld"), out_dir.join("iomem.ld")));
@@ -24,18 +23,17 @@ fn copy_linker_scripts<P: AsRef<Path>>(out_path: P) -> io::Result<()> {
 }
 
 fn main() {
-    let profile = env::var("PROFILE").unwrap();
-    match profile.as_ref() {
-        "dev" | "release" | "debug" => {},
-        _ => {
+    let platform = match get_platform() {
+        Some(p) => p,
+        None => {
             return;
         },
-    }
+    };
     // Get output directory for cargo for zinc crate
     let out_dir = env::var("OUT_DIR").unwrap();
 
     // Move linker scripts to cargo output dir
-    match copy_linker_scripts(&out_dir) {
+    match copy_linker_scripts(&platform, &out_dir) {
         Ok(_) => {},
         Err(e) => panic!("Failed to copy linker scripts: {}", e)
     }
