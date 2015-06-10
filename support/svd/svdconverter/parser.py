b6e3d7a59709ac191065ce8e019a5c2467cf10d4
Paul Osborne <osbpau@gmail.com>
2015-06-10 02:46:41 -0500

    svd: Add unit tests and clean up and fix bugs with parsing code

--- support/svd/svdconverter/parser.py
+++ support/svd/svdconverter/parser.py
@@ -1,77 +1,31 @@
 """SVD XML File Parser"""
 from xml.etree import ElementTree as ET
 
-
-class SVDField(object):
-
-    def __init__(self, name, description, offset, width, access):
-        self.name = name
-        self.description = description
-        self.offset = offset
-        self.width = width
-        self.access = access
-
-    def __repr__(self):
-        return repr(self.__dict__)
+from svdconverter.model import SVDDevice, SVDInterrupt, SVDEnumeratedValue
+from svdconverter.model import SVDPeripheral
+from svdconverter.model import SVDAddressBlock
+from svdconverter.model import SVDRegister
+from svdconverter.model import SVDField
 
 
-class SVDRegister(object):
-
-    def __init__(self, name, description, address_offset, size, access, reset_value, reset_mask, fields):
-        self.name = name
-        self.description = description
-        self.address_offset = address_offset
-        self.size = size
-        self.access = access
-        self.reset_value = reset_value
-        self.reset_mask = reset_mask
-        self.fields = fields
-
-    def __repr__(self):
-        return repr(self.__dict__)
+def _get_text(node, tag, default=None):
+    """Get the text for the provided tag from the provided node"""
+    try:
+        return node.find(tag).text
+    except AttributeError:
+        return default
 
 
-class SVDAddressBlock(object):
-
-    def __init__(self, offset, size, usage):
-        self.offset = offset
-        self.size = size
-        self.usage = usage
-
-    def __repr__(self):
-        return repr(self.__dict__)
-
-
-class SVDPeripheral(object):
-
-    def __init__(self, name, description, prepend_to_name, base_address, address_block, interrupts, registers):
-        self.name = name
-        self.description = description
-        self.prepend_to_name = prepend_to_name
-        self.base_address = base_address
-        self.address_block = address_block
-        self.interrupts = interrupts
-        self.registers = registers
-
-    def __repr__(self):
-        return repr(self.__dict__)
-
-
-class SVDDevice(object):
-
-    def __init__(self, vendor, vendor_id, name, version, description, cpu, bit_offset, bit_width, peripherals):
-        self.vendor = vendor
-        self.vendor_id = vendor_id
-        self.name = name
-        self.version = version
-        self.description = description
-        self.cpu = cpu
-        self.bit_offset = bit_offset
-        self.bit_width = bit_width
-        self.peripherals = peripherals
-
-    def __repr__(self):
-        return repr(self.__dict__)
+def _get_int(node, tag, default=None):
+    text_value = _get_text(node, tag, default)
+    if text_value != default:
+        if text_value.startswith('0x'):
+            return int(text_value[2:], 16)  # hexadecimal
+        elif text_value.startswith('#'):
+            return int(text_value[1:], 2)  # binary
+        else:
+            return int(text_value)  # decimal
+    return default
 
 
 class SVDParser(object):
@@ -85,20 +39,25 @@ class SVDParser(object):
         self._tree = tree
         self._root = self._tree.getroot()
 
-    def _get_text(self, node, tag, default=None):
-        """Get the text for the provided tag from the provided node"""
-        try:
-            return node.find(tag).text
-        except AttributeError:
-            return default
+    def _parse_enumerated_value(self, enumerated_value_node):
+        return SVDEnumeratedValue(
+            name=_get_text(enumerated_value_node, 'name'),
+            description=_get_text(enumerated_value_node, 'description'),
+            value=_get_int(enumerated_value_node, 'value')
+        )
 
     def _parse_field(self, field_node):
+        enumerated_values = []
+        for enumerated_value_node in field_node.findall("./enumeratedValues/enumeratedValue"):
+            enumerated_values.append(self._parse_enumerated_value(enumerated_value_node))
+
         return SVDField(
-            name=self._get_text(field_node, 'name'),
-            description=self._get_text(field_node, 'description'),
-            offset=self._get_text(field_node, 'offset'),
-            width=self._get_text(field_node, 'width'),
-            access=self._get_text(field_node, 'access')
+            name=_get_text(field_node, 'name'),
+            description=_get_text(field_node, 'description'),
+            bit_offset=_get_int(field_node, 'bitOffset'),
+            bit_width=_get_int(field_node, 'bitWidth'),
+            access=_get_text(field_node, 'access'),
+            enumerated_values=enumerated_values or None,
         )
 
     def _parse_register(self, register_node):
@@ -106,35 +65,46 @@ class SVDParser(object):
         for field_node in register_node.findall('.//field'):
             fields.append(self._parse_field(field_node))
         return SVDRegister(
-            name=self._get_text(register_node, 'name'),
-            description=self._get_text(register_node, 'description'),
-            address_offset=self._get_text(register_node, 'addressOffset'),
-            size=self._get_text(register_node, 'size'),
-            access=self._get_text(register_node, 'access'),
-            reset_value=self._get_text(register_node, 'resetValue'),
-            reset_mask=self._get_text(register_node, 'resetMask'),
+            name=_get_text(register_node, 'name'),
+            description=_get_text(register_node, 'description'),
+            address_offset=_get_int(register_node, 'addressOffset'),
+            size=_get_int(register_node, 'size'),
+            access=_get_text(register_node, 'access'),
+            reset_value=_get_int(register_node, 'resetValue'),
+            reset_mask=_get_int(register_node, 'resetMask'),
             fields=fields,
         )
 
     def _parse_address_block(self, address_block_node):
         return SVDAddressBlock(
-            self._get_text(address_block_node, 'offset'),
-            self._get_text(address_block_node, 'size'),
-            self._get_text(address_block_node, 'usage')
+            _get_int(address_block_node, 'offset'),
+            _get_int(address_block_node, 'size'),
+            _get_text(address_block_node, 'usage')
+        )
+
+    def _parse_interrupt(self, interrupt_node):
+        return SVDInterrupt(
+            name=_get_text(interrupt_node, 'name'),
+            value=_get_int(interrupt_node, 'value')
         )
 
     def _parse_peripheral(self, peripheral_node):
         registers = []
-        for register_node in peripheral_node.findall('.//register'):
+        for register_node in peripheral_node.findall('./registers/register'):
             registers.append(self._parse_register(register_node))
+
+        interrupts = []
+        for interrupt_node in peripheral_node.findall('./interrupt'):
+            interrupts.append(self._parse_interrupt(interrupt_node))
+
         address_block = self._parse_address_block(peripheral_node.findall('./addressBlock')[0])
         return SVDPeripheral(
-            name=self._get_text(peripheral_node, 'name'),
-            description=self._get_text(peripheral_node, 'description'),
-            prepend_to_name=self._get_text(peripheral_node, 'prependToName'),
-            base_address=self._get_text(peripheral_node, 'baseAddress'),
+            name=_get_text(peripheral_node, 'name'),
+            description=_get_text(peripheral_node, 'description'),
+            prepend_to_name=_get_text(peripheral_node, 'prependToName'),
+            base_address=_get_int(peripheral_node, 'baseAddress'),
             address_block=address_block,
-            interrupts=None,  # TODO
+            interrupts=interrupts,
             registers=registers,
         )
 
@@ -143,14 +113,14 @@ class SVDParser(object):
         for peripheral_node in device_node.findall('.//peripheral'):
             peripherals.append(self._parse_peripheral(peripheral_node))
         return SVDDevice(
-            vendor=self._get_text(device_node, 'vendor'),
-            vendor_id=self._get_text(device_node, 'vendorID'),
-            name=self._get_text(device_node, 'name'),
-            version=self._get_text(device_node, 'version'),
-            description=self._get_text(device_node, 'description'),
+            vendor=_get_text(device_node, 'vendor'),
+            vendor_id=_get_text(device_node, 'vendorID'),
+            name=_get_text(device_node, 'name'),
+            version=_get_text(device_node, 'version'),
+            description=_get_text(device_node, 'description'),
             cpu=None,  # TODO
-            bit_offset=self._get_text(device_node, 'bitOffset'),
-            bit_width=self._get_text(device_node, 'bitWidth'),
+            address_unit_bits=_get_int(device_node, 'addressUnitBits'),
+            width=_get_int(device_node, 'width'),
             peripherals=peripherals,
         )
 
