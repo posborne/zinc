ee505047c6bd4d7b2ca705fccb52b62a8e82d16b
Paul Osborne <osbpau@gmail.com>
2015-06-15 01:23:18 -0500

    svd: migrate to using cmsis_svd repository for the parser implementation
    The cmsis_svd python package is part of a larger effort in order
    to share CMSIS SVD definitons and parsers amongst varioius projects.
    The package also includes SVDs, but we continue to use our  own
    for the time being until it is clear that we won't need to make
    customizations.

--- support/svd/svdconvert.py
+++ support/svd/svdconvert.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python
 import argparse
 import sys
+from cmsis_svd.parser import SVDParser
 from svdconverter.generators.zinc_ioregs import ZincIoregsCodeGenerator
-from svdconverter.parser import SVDParser
 
 
 def build_arg_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument('path', help="Path to SVD XML File")
     return parser
+
 
 def main():
     args = build_arg_parser().parse_args()
